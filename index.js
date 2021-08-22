497681fde0c0aa9d47b430b22cd159b48b63fcf4
Alexandr <torasov.alexandr@gmail.com>
2021-08-22 13:29:34 +0300

    hw6Done

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const app = new Vue({
     filteredGoods: [],
     searchLine: "Ноутбук",
     basket: {},
+    trying: "ds",
   },
   methods: {
     answer(url) {
@@ -47,7 +48,8 @@ const app = new Vue({
         this.basket.contents.splice(elIndex, 1);
       }
     },
-    filter() {
+    filter(a) {
+      this.searchLine = a;
       this.filteredGoods = this.goods;
       if (this.searchLine) {
         this.filteredGoods = this.filteredGoods.filter((item) => {
@@ -84,170 +86,69 @@ const app = new Vue({
   },
 });
 
-class GoodsItem {
-  constructor({ product_name, price }) {
-    this.title = product_name;
-    this.price = price;
-  }
-  render() {
-    return `<div class="goods-item"><h3>${this.title}</h3><p>${this.price}</p></div>`;
-  }
-}
+Vue.component("goods-list", {
+  props: ["goods"],
+  template: `<div>
+  <h2 class="goods-list">Товары</h2>
+    <div class="goods-list">
+          <goods-item v-for="item in goods" :item="item"></goods-item>
+        </div></div>`,
+});
 
-class GoodsList {
-  constructor(
-    itemClass,
-    place = ".goods-list",
-    catalogData = "/catalogData.json"
-  ) {
-    this.goods = [];
-    this.itemClass = itemClass;
-    this.place = place;
-    this.catalogData = catalogData;
-  }
-  fetchGoods() {
-    return answer(API_URL + this.catalogData).then(
-      (goods) => {
-        this.goods = goods;
+Vue.component("goods-item", {
+  props: ["item"],
+  template: `<div class="goods-item">
+    <h3>{{item.product_name}}</h3>
+    <p>{{item.price}}</p>
+    <button @click="addToCart(item.id_product)">
+      Добавить в корзину
+    </button>
+  </div> `,
+  data() {
+    return {
+      name: "Frodo",
+    };
+  },
+});
 
-        return this.goods;
-      },
-      (error) => {
-        console.log(error);
-      }
-    );
-  }
-  addOneItem() {}
-  removeOneItem() {}
+Vue.component("basket-list", {
+  props: ["goods"],
+  template: `
+  <div>
+  <h2 class="goods-list">Корзина</h2>
+  <div class="goods-list">
+  <basket-item v-for="item in goods" :item="item"></basket-item>
+  </div>
+  </div>`,
+});
 
-  render() {
-    let goodsList = document.querySelector(this.place);
-    this.goods.map((element) => {
-      goodsList.insertAdjacentHTML(
-        "beforeend",
-        new this.itemClass(element).render()
-      );
-    });
-  }
+Vue.component("basket-item", {
+  props: ["item"],
+  template: ` <div class="goods-item" v-if="item.quantity!=0">
+  <h3>{{item.product_name}}</h3>
+  <p>{{item.price}}</p>
+  <p>Количество {{item.quantity}}</p>
+  <button @click="removeFromCart(item.id_product)">X</button>
+</div>
+  `,
+});
 
-  countTotalSum() {
-    return this.goods.reduce((sum, { price }) => sum + price, 0);
-  }
-}
+Vue.component("vue-header", {
+  props: ["search"],
+  data() {
+    return {
+      // searchLine: "Frodo",
+    };
+  },
 
-class CartItem extends GoodsItem {
-  constructor({ product_name, price }) {
-    super({ product_name, price });
-  }
-  render() {
-    return `<div class="goods-item"><h3>${this.title}</h3><p>${this.price}</p><button>X</button></div>`;
-  }
-}
-
-class CartList extends GoodsList {
-  constructor(
-    product_name,
-    place = ".cart-list",
-    catalogData = "/getBasket.json"
-  ) {
-    super(product_name, place, catalogData);
-  }
-
-  fetchGoods() {
-    return answer(API_URL + this.catalogData).then(
-      (goods) => {
-        this.goods = goods.contents;
-
-        return this.goods;
-      },
-      (error) => {
-        console.log(error);
-      }
-    );
-  }
-  clearCart() {}
-  removeAllItems() {}
-
-  // Добавьте в соответствующие классы методы добавления товара в корзину, удаления товара из корзины и получения списка товаров корзины.
-  /*
-https://raw.githubusercontent.com/GeekBrainsTutorial/online-store-api/master/responses – адрес API;
-/catalogData.json – получить список товаров;
-/getBasket.json – получить содержимое корзины;
-/addToBasket.json – добавить товар в корзину;
-/deleteFromBasket.json – удалить товар из корзины.
-*/
-}
-
-/* Некая сеть фастфуда предлагает несколько видов гамбургеров:
-Маленький (50 рублей, 20 калорий).
-Большой (100 рублей, 40 калорий).
-Гамбургер может быть с одним из нескольких видов начинок (обязательно):
-С сыром (+10 рублей, +20 калорий).
-С салатом (+20 рублей, +5 калорий).
-С картофелем (+15 рублей, +10 калорий).
-Дополнительно гамбургер можно посыпать приправой (+15 рублей, +0 калорий) и полить майонезом (+20 рублей, +5 калорий)
-*/
-
-class engridient {
-  constructor(nameEng, price, callories, category = "main") {
-    this.nameEng = nameEng;
-    this.price = price;
-    this.callories = callories;
-    this.category = category;
-  }
-}
-const cheeze = new engridient("cheeze", 10, 20);
-const salad = new engridient("salad", 20, 5);
-const potato = new engridient("potato", 15, 10);
-const spice = new engridient("spice", 15, 0, "topping");
-const mayo = new engridient("mayo", 20, 5, "topping");
-
-class Hamburger {
-  constructor(size = "big", ...engridients) {
-    this.size = size;
-    this.engridients = engridients;
-  }
-  cauntCallories() {
-    return this.engridients.reduce((sum, { callories }) => sum + callories, 0);
-  }
-  cauntPrice() {
-    return this.engridients.reduce((sum, { price }) => sum + price, 0);
-  }
-  get hamburgersize() {
-    return this.size;
-  }
-  get hamburgerEngridients() {
-    let arr = [];
-    this.engridients.map(({ nameEng }) => {
-      arr.push(nameEng);
-    });
-    return arr;
-  }
-  get hamburgerToppings() {
-    let arr = [];
-    this.engridients.map(({ nameEng, category }) => {
-      if (category === "topping") arr.push(nameEng);
-    });
-    return arr;
-  }
-
-  addaddTopping(obj) {
-    if (obj instanceof engridient) {
-      this.engridients.push(obj);
-    }
-  }
-  removeTopping(obj) {
-    if (obj instanceof engridient) {
-      let i = this.engridients.indexOf(obj);
-      if (i != -1) {
-        this.engridients = this.engridients
-          .slice(0, i)
-          .concat(this.engridients.slice(i + 1));
-      }
-    }
-  }
-}
-
-const standartHamburger = ["big", spice, salad, cheeze, cheeze, salad, mayo];
-
-let bk = new Hamburger(...standartHamburger);
+  template: `<div>
+  
+  <header class="header container">
+   <input type="text" class="goods-search" v-model="search" />
+  <button class="search-button" type="button" 
+  v-on:click="$emit('filter-goods', search)">
+   Искать
+ </button>
+  <button class="cart-button" type="button">Корзина</button> </header>
+  </div>`,
+});
